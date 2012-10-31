93e422d14c871ae415e8be2c0fc5a4e4cc7537a8
Shea Levy <shea.levy@logicblox.com>
2012-10-31 19:53:30 -0400

    Add Amazon Download implementation

--- src/com/logicblox/s3lib/Download.java
+++ src/com/logicblox/s3lib/Download.java
@@ -8,7 +8,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 public interface Download {
 	ListenableFuture<InputStream> getPart(long start, long end);
 
-	ListenableFuture<Void> abort();
-
 	Map<String,String> getMeta();
+
+	long getLength();
 }

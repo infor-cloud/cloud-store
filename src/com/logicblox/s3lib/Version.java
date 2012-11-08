0208c8ea8ab6ef3ca8bf9ab91135e65023fc08af
Shea Levy <shea.levy@logicblox.com>
2012-11-08 15:24:36 -0500

    Needed protocol version bump

--- src/com/logicblox/s3lib/Version.java
+++ src/com/logicblox/s3lib/Version.java
@@ -4,5 +4,5 @@ public class Version
 {
   // Version that is used to indicate the format of S3 objects and its
   // annotations. It is not the version of the software.
-  public static final int CURRENT = 2;
+  public static final int CURRENT = 3;
 }

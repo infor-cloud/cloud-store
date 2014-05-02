72034e9da60c0102f18c49e8f7a51b39763ae3bf
Martin Bravenboer <martin.bravenboer@logicblox.com>
2014-05-01 20:55:54 -0700

    whoopsie daisy

---
+++ src/com/logicblox/s3lib/S3File.java
@@ -0,0 +1,51 @@
+package com.logicblox.s3lib;
+
+import java.io.File;
+
+public class S3File
+{
+  private File _localFile;
+  private String _eTag;
+  private String _key;
+  private String _bucket;
+
+  public File getLocalFile()
+  {
+    return _localFile;
+  }
+
+  public void setLocalFile(File f)
+  {
+    _localFile = f;
+  }
+
+  public String getETag()
+  {
+    return _eTag;
+  }
+
+  public void setETag(String tag)
+  {
+    _eTag = tag;
+  }
+
+  public String getKey()
+  {
+    return _key;
+  }
+
+  public void setKey(String key)
+  {
+    _key = key;
+  }
+
+  public String getBucketName()
+  {
+    return _bucket;
+  }
+
+  public void setBucketName(String bucket)
+  {
+    _bucket = bucket;
+  }
+}

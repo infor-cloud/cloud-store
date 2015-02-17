b51166c67dc0e895a59ca236fc2a1cbdf7279ea4
Andy Dean <andy.dean@logicblox.com>
2015-02-17 15:13:41 -0500

    KIAB-707 Updated instructions for getting new version of s3lib jar and running the download.  Cleaned up code for file download example.

--- s3lib-example/src/main/java/com/logicblox/S3downloader.java
+++ s3lib-example/src/main/java/com/logicblox/S3downloader.java
@@ -1,117 +1,124 @@
 package com.logicblox;
 
-import com.logicblox.s3lib.*;
-
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
-import com.google.common.util.concurrent.MoreExecutors;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.ListeningScheduledExecutorService;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.ListeningScheduledExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.logicblox.s3lib.DirectoryKeyProvider;
+import com.logicblox.s3lib.KeyProvider;
+import com.logicblox.s3lib.S3Client;
+import com.logicblox.s3lib.UsageException;
+import com.logicblox.s3lib.Utils;
 
 public class S3downloader {
 
-	private static ListeningExecutorService getHttpExecutor() {
-		int maxConcurrentConnections = 10;
-		return MoreExecutors.listeningDecorator(Executors
-				.newFixedThreadPool(maxConcurrentConnections));
-	}
+    private static ListeningExecutorService getHttpExecutor() {
+        int maxConcurrentConnections = 10;
+        return MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(maxConcurrentConnections));
+    }
 
-	private static ListeningScheduledExecutorService getInternalExecutor() {
-		return MoreExecutors.listeningDecorator(Executors
-				.newScheduledThreadPool(50));
-	}
+    private static ListeningScheduledExecutorService getInternalExecutor() {
+        return MoreExecutors.listeningDecorator(Executors.newScheduledThreadPool(50));
+    }
 
-	private static KeyProvider getKeyProvider() {
-		String encKeyDirectory = Utils.getDefaultKeyDirectory();
-		File dir = new File(encKeyDirectory);
-		if (!dir.exists() && !dir.mkdirs())
-			throw new UsageException("specified key directory '"
-					+ encKeyDirectory + "' does not exist");
+    private static KeyProvider getKeyProvider() {
+        String encKeyDirectory = Utils.getDefaultKeyDirectory();
+        File dir = new File(encKeyDirectory);
+        if (!dir.exists() && !dir.mkdirs()) {
+            throw new UsageException(String.format("specified key directory '%s' does not exist", encKeyDirectory));
+        }
+        if (!dir.isDirectory()) {
+            throw new UsageException(String.format("specified key directory '%s' is not a directory", encKeyDirectory));
+        }
+        return new DirectoryKeyProvider(dir);
+    }
 
-		if (!dir.isDirectory())
-			throw new UsageException("specified key directory '"
-					+ encKeyDirectory + "' is not a directory");
+    protected static URI getURI(List<String> urls) throws URISyntaxException {
+        if (urls.size() != 1) {
+            throw new UsageException("A single S3 object URL is required");
+        }
+        return Utils.getURI(urls.get(0));
+    }
 
-		return new DirectoryKeyProvider(dir);
-	}
+    protected static String getBucket(List<String> urls) throws URISyntaxException {
+        return Utils.getBucket(getURI(urls));
+    }
 
-	protected static URI getURI(List<String> urls) throws URISyntaxException {
-		if (urls.size() != 1)
-			throw new UsageException("A single S3 object URL is required");
-		return Utils.getURI(urls.get(0));
-	}
+    protected static String getObjectKey(List<String> urls) throws URISyntaxException {
+        return Utils.getObjectKey(getURI(urls));
+    }
 
-	protected static String getBucket(List<String> urls)
-			throws URISyntaxException {
-		return Utils.getBucket(getURI(urls));
-	}
+    private static void download(S3Client client, List<String> urls) throws Exception {
+        String file = "test.gz";
+        File output = new File(file);
+        ListenableFuture<?> result;
+        boolean recursive = false;
+        boolean overwrite = true;
 
-	protected static String getObjectKey(List<String> urls)
-			throws URISyntaxException {
-		return Utils.getObjectKey(getURI(urls));
-	}
+        if (getObjectKey(urls).endsWith("/")) {
+            result = client.downloadDirectory(output, getURI(urls), recursive, overwrite);
+        } else {
+            // Test if S3 url exists.
+            if (client.exists(getBucket(urls), getObjectKey(urls)).get() == null) {
+                throw new UsageException("Object not found at " + getURI(urls));
+            }
+            result = client.download(output, getURI(urls));
+        }
 
-	private static void download(S3Client client, List<String> urls)
-			throws Exception {
-		String file = "test.gz";
-		File output = new File(file);
-		ListenableFuture<?> result;
-		boolean recursive = false;
-		boolean overwrite = true;
+        try {
+            result.get();
+        } catch (ExecutionException exc) {
+            rethrow(exc.getCause());
+        }
 
-		if (getObjectKey(urls).endsWith("/")) {
-			result = client.downloadDirectory(output, getURI(urls), recursive,
-					overwrite);
-		} else {
-			// Test if S3 url exists.
-			if (client.exists(getBucket(urls), getObjectKey(urls)).get() == null) {
-				throw new UsageException("Object not found at " + getURI(urls));
-			}
-			result = client.download(output, getURI(urls));
-		}
+        client.shutdown();
+    }
 
-		try {
-			result.get();
-		} catch (ExecutionException exc) {
-			rethrow(exc.getCause());
-		}
+    private static void rethrow(Throwable thrown) throws Exception {
+        if (thrown instanceof Exception) {
+            throw (Exception) thrown;
+        }
+        if (thrown instanceof Error) {
+            throw (Error) thrown;
+        } else {
+            throw new RuntimeException(thrown);
+        }
+    }
 
-		client.shutdown();
-	}
 
-	private static void rethrow(Throwable thrown) throws Exception {
-		if (thrown instanceof Exception)
-			throw (Exception) thrown;
-		if (thrown instanceof Error)
-			throw (Error) thrown;
-		else
-			throw new RuntimeException(thrown);
-	}
+    /*
+     * This is a bare bones implementation of downloading a file from S3. No error
+     * checking for things like
+     *   AWS_ACCESS_KEY_ID defined?
+     *   AWS_SECRET_KEY defined?
+     *   s3lib-keys directory defined?
+     *   file being requested actually exists?
+     */
 
-	public static void main(String[] args) throws Exception {
-		ClientConfiguration clientCfg = new ClientConfiguration();
-		clientCfg.setProtocol(Protocol.HTTPS);
+    public static void main(String[] args) throws Exception {
+        ClientConfiguration clientCfg = new ClientConfiguration();
+        clientCfg.setProtocol(Protocol.HTTPS);
 
-	
-		AmazonS3Client s3Client = new AmazonS3Client(clientCfg);
+        AmazonS3Client s3Client = new AmazonS3Client(clientCfg);
 
-		long chunkSize = Utils.getDefaultChunkSize();
+        long chunkSize = Utils.getDefaultChunkSize();
 
-		S3Client client = new S3Client(s3Client, getHttpExecutor(),
-				getInternalExecutor(), chunkSize, getKeyProvider());
-		List<String> urls = new ArrayList<String>();
-		urls.add("s3://kiabi-fred-dev/fmachine/test.gz");
-		download(client, urls);
-		client.shutdown();
-	}
+        S3Client client = new S3Client(s3Client, getHttpExecutor(), getInternalExecutor(), chunkSize, getKeyProvider());
+        List<String> urls = new ArrayList<String>();
+        urls.add("s3://kiabi-fred-dev/fmachine/test.gz");
+        System.out.println("Downloading test.gz");
+        download(client, urls);
+        client.shutdown();
+    }
 }

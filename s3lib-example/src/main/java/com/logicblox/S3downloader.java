ba3425e920fa6b79bdd770288ed92040690be41c
George Kollias <george.kollias@logicblox.com>
2015-03-27 18:00:09 +0200

    Updated examples: - Removed specific customer references - Better structure - Explicit handling of futures - Asserts to force user add custom values + examples

--- s3lib-example/src/main/java/com/logicblox/S3downloader.java
+++ s3lib-example/src/main/java/com/logicblox/S3downloader.java
@@ -2,6 +2,8 @@ package com.logicblox;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
@@ -11,30 +13,11 @@ import com.logicblox.s3lib.CloudStoreClient;
 import com.logicblox.s3lib.DownloadOptions;
 import com.logicblox.s3lib.DownloadOptionsBuilder;
 import com.logicblox.s3lib.S3Client;
-import com.logicblox.s3lib.UsageException;
+import com.logicblox.s3lib.S3File;
 import com.logicblox.s3lib.Utils;
 
 
 public class S3downloader {
-
-    private static void download(CloudStoreClient client,
-                                 DownloadOptions options)
-        throws Exception {
-        URI url = client.getUri(options.getBucket(), options.getObjectKey());
-
-        ListenableFuture<?> result;
-        if (options.getObjectKey().endsWith("/")) {
-            result = client.downloadDirectory(options);
-        } else {
-            // Test if url exists.
-            if (client.exists(url).get() == null) {
-                throw new UsageException("Object not found at " + url);
-            }
-            result = client.download(options);
-        }
-
-        result.get();
-    }
 
     /*
      * This is a bare bones implementation of downloading a file from S3. No
@@ -45,38 +28,63 @@ public class S3downloader {
      */
 
     public static void main(String[] args) throws Exception {
-        // Define the client configuration
+        CloudStoreClient client = getCloudStoreClient();
+        DownloadOptions options = getDownloadOptions();
+
+        // Download the file/directory
+        System.out.println(String.format("Downloading '%s' to '%s'.",
+            options.getObjectKey(), options.getFile()));
+        if (options.getObjectKey().endsWith("/")) {
+            ListenableFuture<List<S3File>> result =
+                client.downloadDirectory(options);
+            List<S3File> fs = result.get();
+        } else {
+            ListenableFuture<S3File> result =
+                client.download(options);
+            S3File f = result.get();
+        }
+
+        client.shutdown();
+    }
+
+    private static CloudStoreClient getCloudStoreClient() {
+        // Client configuration
         ClientConfiguration clientCfg = new ClientConfiguration();
-        clientCfg.setProtocol(Protocol.HTTPS);
-        // Add proxy connection if needed:
-        //    clientCfg.setProxyHost("localhost");
-        //    clientCfg.setProxyPort(8118);
+        // Examples of configuration options:
+        //   clientCfg.setProtocol(Protocol.HTTPS);
+        //   clientCfg.setProxyHost("localhost");
+        //   clientCfg.setProxyPort(8118);
         AmazonS3Client s3Client = new AmazonS3Client(clientCfg);
 
-        // Encryption key directory.
-        // String key_dir = Utils.getDefaultKeyDirectory()
-        String key_dir = System.getProperty("user.home") + File.separator +
-            ".s3lib-keys";
+        // Encryption key directory. Examples:
+        //   String key_dir = Utils.getDefaultKeyDirectory();
+        //   String key_dir = System.getProperty("user.home") + File
+        //     .separator + ".s3lib-keys";
+        String key_dir = null;
+        assert key_dir != null;
 
         // CloudStore client
-        CloudStoreClient client = new S3Client(s3Client,
-            Utils.getKeyProvider(key_dir));
+        return new S3Client(s3Client, Utils.getKeyProvider(key_dir));
+    }
 
+    private static DownloadOptions getDownloadOptions() throws URISyntaxException {
         // Set download options
-        URI url = Utils.getURI("s3://kiabi-s3-poc/test/test.dat.gz");
-        File file = new File("test-downloaded.gz");
-        System.out.println(String.format("Downloading '%s' to '%s'.",
-            url, file));
 
-        DownloadOptions options = new DownloadOptionsBuilder()
+        // S3 object URI. Example:
+        //   URI url = Utils.getURI("s3://bucket/dir/test.txt");
+        URI url = null;
+        assert url != null;
+
+        // File to upload. Example:
+        //   File file = new File("/path/to/test.txt");
+        File file = null;
+        assert file != null;
+
+        return new DownloadOptionsBuilder()
             .setFile(file)
             .setUri(url)
             .setRecursive(false)
             .setOverwrite(true)
             .createDownloadOptions();
-
-        // Download the file
-        download(client, options);
-        client.shutdown();
     }
 }
--- s3lib-example/src/main/java/com/logicblox/S3uploader.java
+++ s3lib-example/src/main/java/com/logicblox/S3uploader.java
@@ -2,13 +2,17 @@ package com.logicblox;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.logicblox.s3lib.CloudStoreClient;
 import com.logicblox.s3lib.S3Client;
+import com.logicblox.s3lib.S3File;
 import com.logicblox.s3lib.UploadOptions;
 import com.logicblox.s3lib.UploadOptionsBuilder;
 import com.logicblox.s3lib.UsageException;
@@ -16,8 +20,83 @@ import com.logicblox.s3lib.Utils;
 
 public class S3uploader {
 
-    static String encKeyName = "kiabi-encryption-key";
-    static String cannedAcl = "bucket-owner-full-control";
+    public static void main(String[] args) throws Exception {
+        CloudStoreClient client = getCloudStoreClient();
+        UploadOptions options = getUploadOptions();
+
+        // Upload the file/directory
+        if (options.getObjectKey().endsWith("/")) {
+            throw new UsageException("Destination key " + options.getObjectKey()
+                + " should be fully qualified. No trailing '/' is permitted.");
+        }
+
+        if (options.getFile().isFile()) {
+            ListenableFuture<S3File> result = client.upload(options);
+            S3File f = result.get();
+        } else if (options.getFile().isDirectory()) {
+            ListenableFuture<List<S3File>> result  =
+                client.uploadDirectory(options);
+            List<S3File> fs = result.get();
+        } else {
+            throw new UsageException("File '" + options.getFile() + "' is not" +
+                " a file or a directory.");
+        }
+
+        client.shutdown();
+    }
+
+    private static CloudStoreClient getCloudStoreClient() {
+        // Client configuration
+        ClientConfiguration clientCfg = new ClientConfiguration();
+        // Examples of configuration options:
+        //    clientCfg.setProtocol(Protocol.HTTPS);
+        //    clientCfg.setProxyHost("localhost");
+        //    clientCfg.setProxyPort(8118);
+        AmazonS3Client s3Client = new AmazonS3Client(clientCfg);
+
+        // Encryption key directory. Examples:
+        //   String key_dir = Utils.getDefaultKeyDirectory()
+        //   String key_dir = System.getProperty("user.home") + File.separator +
+        //     ".s3lib-keys";
+        String key_dir = null;
+        assert key_dir != null;
+
+        // CloudStore client
+        return new S3Client(s3Client, Utils.getKeyProvider(key_dir));
+    }
+
+    private static UploadOptions getUploadOptions() throws URISyntaxException {
+        // Set upload options
+
+        // S3 object URI. Example:
+        //   URI url = Utils.getURI("s3://bucket/dir/test.txt");
+        URI url = null;
+        assert url != null;
+
+        // Save file to. Example:
+        //   File file = new File("/path/to/test.txt");
+        File file = null;
+        assert file != null;
+
+        // Encryption key-pair name. Example:
+        //   String encKeyName = "enc-key";
+        String encKeyName = null;
+
+        // Permissions. Example:
+        //   String cannedAcl = "bucket-owner-full-control";
+        String cannedAcl = null;
+        assert cannedAcl != null;
+
+        if (!isValidS3Acl(cannedAcl))
+            throw new UsageException("Unknown canned ACL '" + cannedAcl + "'");
+
+        return new UploadOptionsBuilder()
+            .setFile(file)
+            .setUri(url)
+            .setEncKey(encKeyName)
+            .setAcl(cannedAcl)
+            .createUploadOptions();
+    }
 
     private static boolean isValidS3Acl(String value) {
         for (CannedAccessControlList acl : CannedAccessControlList.values()) {
@@ -26,65 +105,4 @@ public class S3uploader {
         }
         return false;
     }
-
-    public static void upload(CloudStoreClient client, UploadOptions options)
-        throws Exception {
-
-        if (options.getObjectKey().endsWith("/")) {
-            URI url = client.getUri(options.getBucket(), options.getObjectKey());
-            throw new UsageException("Destination key " + url + " should be " +
-                "fully qualified. No trailing '/' is permitted.");
-        }
-
-        if (options.getFile().isFile()) {
-            client.upload(options).get();
-        } else if (options.getFile().isDirectory()) {
-            client.uploadDirectory(options).get();
-        } else {
-            throw new UsageException("File '" + options.getFile() + "' is not" +
-                " a file or a directory.");
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        // Define the client configuration
-        ClientConfiguration clientCfg = new ClientConfiguration();
-        clientCfg.setProtocol(Protocol.HTTPS);
-        // Add proxy connection if needed:
-        //    clientCfg.setProxyHost("localhost");
-        //    clientCfg.setProxyPort(8118);
-        AmazonS3Client s3Client = new AmazonS3Client(clientCfg);
-
-        // Encryption key directory.
-        // String key_dir = Utils.getDefaultKeyDirectory()
-        String key_dir = System.getProperty("user.home") + File.separator +
-            ".s3lib-keys";
-
-        // CloudStore client
-        CloudStoreClient client = new S3Client(s3Client,
-            Utils.getKeyProvider(key_dir));
-
-        // Set upload options
-        URI url = Utils.getURI("s3://kiabi-s3-poc/test/test.dat.gz");
-        File file = new File("test.dat.gz");
-        System.out.println(String.format("Encryption key is '%s'.",
-            encKeyName));
-        System.out.println(String.format("Uploading '%s' to '%s'.", file, url));
-
-        if (!isValidS3Acl(cannedAcl))
-            throw new UsageException("Unknown canned ACL '" + cannedAcl + "'");
-
-        UploadOptions options = new UploadOptionsBuilder()
-            .setFile(file)
-            .setUri(url)
-            .setEncKey(encKeyName)
-            .setAcl(cannedAcl)
-            .createUploadOptions();
-
-        // Upload the file
-        upload(client, options);
-        client.shutdown();
-    }
-
-
 }

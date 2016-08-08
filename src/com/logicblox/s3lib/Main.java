ec0fa71324a4feb78fe9c0f41dc195fb5a4f6fbb
waad.aljaradt <devnull@localhost>
2016-08-08 15:19:59 -0400

    remove import of HashMap

--- src/com/logicblox/s3lib/Main.java
+++ src/com/logicblox/s3lib/Main.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.Map;

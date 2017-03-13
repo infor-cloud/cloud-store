76ff7a158337f9e319597e4af4b066bda636d0c8
Wes Hunter <wes.hunter@logicblox.com>
2017-03-13 13:29:02 -0400

    added more upload/download tests

--- test/src/com/logicblox/s3lib/UploadDownloadTests.java
+++ test/src/com/logicblox/s3lib/UploadDownloadTests.java
@@ -6,11 +6,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.net.URI;
 import java.nio.file.Files;
+import java.nio.file.FileVisitResult;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.Random;
 import junit.framework.Assert;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -25,6 +28,7 @@ public class UploadDownloadTests
 {
   private static CloudStoreClient _client = null;
   private static String _testBucket = null;
+  private static Random _rand = null;
 
 
   @BeforeClass
@@ -33,6 +37,7 @@ public class UploadDownloadTests
   {
     _client = TestOptions.createClient(0);
     _testBucket = TestOptions.createBucket(_client);
+    _rand = new Random(System.currentTimeMillis());
   }
 
 
@@ -51,54 +56,27 @@ public class UploadDownloadTests
   public void testSimpleUploadDownload()
     throws Throwable
   {
-    // should be no files initially in bucket
-    ListOptions lsOpts = new ListOptionsBuilder()
-      .setBucket(_testBucket)
-      .setRecursive(true)
-      .setIncludeVersions(false)
-      .setExcludeDirs(false)
-      .createListOptions();
-    List<S3File> objs = _client.listObjects(lsOpts).get();
+    List<S3File> objs = listObjects();
     int originalCount = objs.size();
 
     // create a small file and upload it
     File toUpload = createTextFile(100);
     String rootPrefix = "a/b/";
     URI dest = getUri(toUpload, rootPrefix);
-    UploadOptions upOpts = new UploadOptionsBuilder()
-      .setFile(toUpload)
-      .setBucket(Utils.getBucket(dest))
-      .setObjectKey(Utils.getObjectKey(dest))
-      .createUploadOptions();
-    S3File f = _client.upload(upOpts).get();
+    S3File f = uploadFile(toUpload, dest);
     Assert.assertNotNull(f);
 
     // make sure file was uploaded
-    objs = _client.listObjects(lsOpts).get();
+    objs = listObjects();
     Assert.assertEquals(originalCount + 1, objs.size());
     String key = rootPrefix + toUpload.getName();
-    boolean found = false;
-    for(S3File o : objs)
-    {
-      if(o.getKey().equals(key))
-      {
-        found = true;
-        break;
-      }
-    }
-    Assert.assertTrue(found);
+    Assert.assertTrue(findObject(objs, key));
 
     // download the file and compare it with the original
     File dlTemp = createTmpFile();
-    DownloadOptions dlOpts = new DownloadOptionsBuilder()
-      .setFile(dlTemp)
-      .setUri(dest)
-      .setRecursive(false)
-      .setOverwrite(true)
-      .createDownloadOptions();
-    f = _client.download(dlOpts).get();
+    f = downloadFile(dest, dlTemp);
     Assert.assertNotNull(f.getLocalFile());
-    compareFiles(toUpload, f.getLocalFile());
+    Assert.assertTrue(compareFiles(toUpload, f.getLocalFile()));
 
     // test a few other misc cloud-store functions
     Assert.assertNotNull(_client.exists(dest).get());
@@ -120,13 +98,10 @@ public class UploadDownloadTests
     // create a small file and upload it
     long fileSize = 100;
     File toUpload = createTextFile(fileSize);
+
+    Assert.assertEquals(fileSize, toUpload.length());
     URI dest = getUri(toUpload, "");
-    UploadOptions upOpts = new UploadOptionsBuilder()
-      .setFile(toUpload)
-      .setBucket(Utils.getBucket(dest))
-      .setObjectKey(Utils.getObjectKey(dest))
-      .createUploadOptions();
-    S3File f = _client.upload(upOpts).get();
+    S3File f = uploadFile(toUpload, dest);
     Assert.assertNotNull(f);
     Assert.assertEquals(
       toUpload.getAbsolutePath(), f.getLocalFile().getAbsolutePath());
@@ -140,13 +115,7 @@ public class UploadDownloadTests
     Assert.assertFalse(f.getTimestamp().isPresent());
 // WHY?
 
-    ListOptions lsOpts = new ListOptionsBuilder()
-      .setBucket(_testBucket)
-      .setRecursive(true)
-      .setIncludeVersions(false)
-      .setExcludeDirs(false)
-      .createListOptions();
-    List<S3File> objs = _client.listObjects(lsOpts).get();
+    List<S3File> objs = listObjects();
     for(S3File o : objs)
     {
       if(o.getKey() == toUpload.getName())
@@ -179,22 +148,11 @@ public class UploadDownloadTests
     long fileSize = 100;
     File toUpload = createTextFile(fileSize);
     URI dest = getUri(toUpload, "");
-    UploadOptions upOpts = new UploadOptionsBuilder()
-      .setFile(toUpload)
-      .setBucket(Utils.getBucket(dest))
-      .setObjectKey(Utils.getObjectKey(dest))
-      .createUploadOptions();
-    _client.upload(upOpts).get();
+    uploadFile(toUpload, dest);
 
     // now download it
     File dlTemp = createTmpFile();
-    DownloadOptions dlOpts = new DownloadOptionsBuilder()
-      .setFile(dlTemp)
-      .setUri(dest)
-      .setRecursive(false)
-      .setOverwrite(true)
-      .createDownloadOptions();
-    S3File f = _client.download(dlOpts).get();
+    S3File f = downloadFile(dest, dlTemp);
     Assert.assertNotNull(f);
     Assert.assertEquals(
       dlTemp.getAbsolutePath(), f.getLocalFile().getAbsolutePath());
@@ -210,15 +168,244 @@ public class UploadDownloadTests
   }
 
 
-  private void compareFiles(File f1, File f2)
+  @Test
+  public void testEmptyFile()
+    throws Throwable
+  {
+    List<S3File> objs = listObjects();
+    int originalCount = objs.size();
+
+    // upload a file
+    File toUpload = createTextFile(0);
+    Assert.assertEquals(0, toUpload.length());
+    URI dest = getUri(toUpload, "");
+    S3File f = uploadFile(toUpload, dest);
+    Assert.assertNotNull(f);
+
+    // make sure file was uploaded
+    objs = listObjects();
+    Assert.assertEquals(originalCount + 1, objs.size());
+    String key = toUpload.getName();
+    Assert.assertTrue(findObject(objs, key));
+
+    // download, overwriting a larger file
+    File dlTemp = createTextFile(100);
+    Assert.assertEquals(100, dlTemp.length());
+    f = downloadFile(dest, dlTemp);
+    Assert.assertEquals(0, dlTemp.length());
+  }
+
+
+  @Test
+  public void testDownloadNoOverwriteFile()
+    throws Throwable
+  {
+    List<S3File> objs = listObjects();
+    int originalCount = objs.size();
+
+    // upload a file
+    File toUpload = createTextFile(100);
+    URI dest = getUri(toUpload, "");
+    S3File f = uploadFile(toUpload, dest);
+    Assert.assertNotNull(f);
+
+    // make sure file was uploaded
+    objs = listObjects();
+    Assert.assertEquals(originalCount + 1, objs.size());
+    String key = toUpload.getName();
+    Assert.assertTrue(findObject(objs, key));
+
+    // download without overwrite to make sure it fails
+    File dlTemp = createTextFile(100);
+    Assert.assertFalse(compareFiles(toUpload, dlTemp));
+    try
+    {
+      f = downloadFile(dest, dlTemp, false);
+      Assert.fail("Expected download exception");
+    }
+    catch(Throwable t)
+    {
+      // expected
+    }
+
+    // now download with overwite to make sure it replaces the file
+    f = downloadFile(dest, dlTemp, true);
+    Assert.assertTrue(compareFiles(toUpload, dlTemp));
+  }
+
+
+  @Test
+  public void testDirectoryUploadDownload()
+    throws Throwable
+  {
+    File top = null;
+    File dlDir = null;
+    File dlDir2 = null;
+    try
+    {
+      // create simple directory structure with a few files
+      top = createTmpDir();
+      File a = createTextFile(top, 100);
+      File b = createTextFile(top, 100);
+      File sub = createTmpDir(top);
+      File c = createTextFile(sub, 100);
+      File d = createTextFile(sub, 100);
+      File sub2 = createTmpDir(sub);
+      File e = createTextFile(sub2, 100);
+
+      List<S3File> objs = listObjects();
+      int originalCount = objs.size();
+
+      // upload the directory
+      String rootPrefix = "a/b/";
+      URI dest = getUri(top, rootPrefix);
+      List<S3File> uploaded = uploadDir(top, dest);
+      Assert.assertEquals(5, uploaded.size());
+
+      // verify that the structure was replicated
+      objs = listObjects();
+      Assert.assertEquals(originalCount + 5, objs.size());
+      String topN = rootPrefix + top.getName() + "/";
+      String subN = topN + sub.getName() + "/";
+      String sub2N = subN + sub2.getName() + "/";
+      Assert.assertTrue(findObject(objs, topN + a.getName()));
+      Assert.assertTrue(findObject(objs, topN + b.getName()));
+      Assert.assertTrue(findObject(objs, subN + c.getName()));
+      Assert.assertTrue(findObject(objs, subN + d.getName()));
+      Assert.assertTrue(findObject(objs, sub2N + e.getName()));
+
+      // non-recursive directory download
+      dlDir = createTmpDir();
+      List<S3File> downloaded = downloadDir(dest, dlDir, false);
+      Assert.assertEquals(2, downloaded.size());
+      Assert.assertEquals(2, dlDir.list().length);
+      Assert.assertTrue(compareFiles(a, new File(dlDir, a.getName())));
+      Assert.assertTrue(compareFiles(b, new File(dlDir, b.getName())));
+
+      // recursive directory download
+      dlDir2 = createTmpDir();
+      downloaded = downloadDir(dest, dlDir2, true);
+      Assert.assertEquals(5, downloaded.size());
+      Assert.assertEquals(3, dlDir2.list().length);
+      Assert.assertTrue(compareFiles(a, new File(dlDir2, a.getName())));
+      Assert.assertTrue(compareFiles(b, new File(dlDir2, b.getName())));
+
+      File dlsub = new File(dlDir2, sub.getName());
+      Assert.assertTrue(dlsub.exists() && dlsub.isDirectory());
+      Assert.assertEquals(3, dlsub.list().length);
+      Assert.assertTrue(compareFiles(c, new File(dlsub, c.getName())));
+      Assert.assertTrue(compareFiles(d, new File(dlsub, d.getName())));
+
+      File dlsub2 = new File(dlsub, sub2.getName());
+      Assert.assertTrue(dlsub2.exists() && dlsub2.isDirectory());
+      Assert.assertEquals(1, dlsub2.list().length);
+      Assert.assertTrue(compareFiles(e, new File(dlsub2, e.getName())));
+
+    }
+    finally
+    {
+      destroyDir(top);
+      destroyDir(dlDir);
+      destroyDir(dlDir2);
+    }
+  }
+
+
+  private boolean findObject(List<S3File> objs, String key)
+  {
+    for(S3File o : objs)
+    {
+      if(o.getKey().equals(key))
+        return true;
+    }
+    return false;
+  }
+
+
+  private S3File uploadFile(File src, URI dest)
+    throws Throwable
+  {
+    UploadOptions upOpts = new UploadOptionsBuilder()
+      .setFile(src)
+      .setBucket(Utils.getBucket(dest))
+      .setObjectKey(Utils.getObjectKey(dest))
+      .createUploadOptions();
+    return _client.upload(upOpts).get();
+  }
+
+
+  private List<S3File> uploadDir(File src, URI dest)
+    throws Throwable
+  {
+    UploadOptions upOpts = new UploadOptionsBuilder()
+      .setFile(src)
+      .setBucket(Utils.getBucket(dest))
+      .setObjectKey(Utils.getObjectKey(dest))
+      .createUploadOptions();
+    return _client.uploadDirectory(upOpts).get();
+  }
+
+
+  private S3File downloadFile(URI src, File dest)
+    throws Throwable
+  {
+    return downloadFile(src, dest, true);
+  }
+
+
+  private S3File downloadFile(URI src, File dest, boolean overwrite)
+    throws Throwable
+  {
+    DownloadOptions dlOpts = new DownloadOptionsBuilder()
+      .setFile(dest)
+      .setUri(src)
+      .setRecursive(false)
+      .setOverwrite(overwrite)
+      .createDownloadOptions();
+    return _client.download(dlOpts).get();
+  }
+
+
+  private List<S3File> downloadDir(URI src, File dest, boolean recursive)
+    throws Throwable
+  {
+    DownloadOptions dlOpts = new DownloadOptionsBuilder()
+      .setFile(dest)
+      .setUri(src)
+      .setRecursive(recursive)
+      .setOverwrite(true)
+      .createDownloadOptions();
+    return _client.downloadDirectory(dlOpts).get();
+  }
+
+
+  private List<S3File> listObjects()
+    throws Throwable
+  {
+    ListOptions lsOpts = new ListOptionsBuilder()
+      .setBucket(_testBucket)
+      .setRecursive(true)
+      .setIncludeVersions(false)
+      .setExcludeDirs(false)
+      .createListOptions();
+    return _client.listObjects(lsOpts).get();
+  }
+
+
+  private boolean compareFiles(File f1, File f2)
     throws FileNotFoundException, IOException
   {
-    Assert.assertEquals(f1.length(), f2.length());
+    if(f1.length() != f2.length())
+      return false;
     FileReader r1 = new FileReader(f1);
     FileReader r2 = new FileReader(f2);
     int c = -1;
     while((c = r1.read()) != -1)
-      Assert.assertEquals(c, r2.read());
+    {
+      if(c != r2.read())
+        return false;
+    }
+    return true;
   }
 
 
@@ -229,18 +416,28 @@ public class UploadDownloadTests
       prefix = "/" + prefix;
     if(!prefix.endsWith("/"))
       prefix = prefix + "/";
-    return new URI(TestOptions.getService() + "://" + _testBucket + prefix + src.getName());
+    String end = "";
+    if(src.isDirectory())
+      end = "/";
+    return new URI(TestOptions.getService() + "://" + _testBucket + prefix + src.getName() + end);
   }
 
 
   private File createTextFile(long size)
     throws IOException
   {
-    File f = createTmpFile(".txt");
+    return createTextFile(null, size);
+  }
+
+
+  private File createTextFile(File parent, long size)
+    throws IOException
+  {
+    File f = createTmpFile(parent, ".txt");
     FileWriter fw = new FileWriter(f);
-    int a = 'a';
+    int start = _rand.nextInt(26) + (int) 'a';
     for(long i = 0; i < size; ++i)
-      fw.append((char) (a + (i % 26)));
+      fw.append((char) (((start + i) % 26) + 'a'));
     fw.close();
     return f;
   }
@@ -249,17 +446,58 @@ public class UploadDownloadTests
   private File createTmpFile()
     throws IOException
   {
-    return createTmpFile(null);
+    return createTmpFile(null, null);
   }
 
 
-  private File createTmpFile(String ext)
+  private File createTmpFile(File parent, String ext)
     throws IOException
   {
-    File f = File.createTempFile("cloud-store-ut", ext);
+    File f = File.createTempFile("cloud-store-ut", ext, parent);
     f.deleteOnExit();
     return f;
   }
 
+
+  private File createTmpDir()
+    throws IOException
+  {
+    return Files.createTempDirectory("cloud-store-ut").toFile();
+  }
+
+
+  private File createTmpDir(File parent)
+    throws IOException
+  {
+    return Files.createTempDirectory(parent.toPath(), "cloud-store-ut").toFile();
+  }
+
+
+  private void destroyDir(File dirF)
+    throws IOException
+  {
+    if((null == dirF) || !dirF.exists())
+      return;
+
+    Path directory = dirF.toPath();
+    Files.walkFileTree(directory, new SimpleFileVisitor<Path>() 
+    {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) 
+        throws IOException 
+      {
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException exc) 
+        throws IOException
+      {
+        Files.delete(dir);
+        return FileVisitResult.CONTINUE;
+      }
+    });
+  }
 
 }

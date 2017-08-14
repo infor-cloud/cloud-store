66f369283dabcc36ea70238c47357f254985d1ab
Wes Hunter <wes.hunter@logicblox.com>
2017-08-14 13:15:26 -0400

    trying to disable cloud-store tests for OSX

--- default.nix
+++ default.nix
@@ -20,6 +20,76 @@ let
   revision = version s3lib;
   name = "s3lib-${revision}";
 
+  build_minio = pkgs.buildGoPackage rec {
+    name = "minio";
+    goPackagePath = "github.com/minio/minio";
+    rev = "e2aba9196f849c458303aff42d2d6ea3e3ea8904";
+
+    src = pkgs.fetchgit {
+      inherit rev;
+      url = "https://github.com/minio/minio.git";
+      sha256 = "1iixpxcyhfa1lln3qd4xpnmjpbkf0zicj1irk21wqjqkac3rar0s";
+    };
+  };
+
+  test_linux =
+    pkgs.stdenv.mkDerivation {
+      name = "${name}-test";
+      src = jobs.build.out;
+      jre = "${jdk.jre}";
+      buildInputs =
+        [
+          pkgs.awscli
+          jobs.build
+          build_minio
+        ];
+      buildPhase = ''
+        set -e
+        minio_pid=""
+
+       cleanup_minio()
+        {
+          if [[ -n "$minio_pid" ]];
+          then
+            kill $minio_pid
+          fi
+        }
+
+        trap cleanup_minio EXIT
+
+        export HOME=$(pwd)
+
+        # these are dummy keys created just for the tests
+        export AWS_ACCESS_KEY_ID=9NLZKB4SPH2OP5L845XE
+        export AWS_SECRET_ACCESS_KEY=rvzui7pQS0PI1aAOhtTHWVmJvhMY+b9xSw7arAbC
+
+        s3_addr="127.0.0.1:9000"
+        s3_endpoint="http://$s3_addr/"
+        keydir="$(pwd)/cloud-store-ut-keys"
+        mkdir -p $keydir
+
+        minio_bin="${build_minio}/bin/minio"
+        $minio_bin -h
+        $minio_bin version
+
+        MINIO_ACCESS_KEY=$AWS_ACCESS_KEY_ID MINIO_SECRET_KEY=$AWS_SECRET_ACCESS_KEY \
+          $minio_bin server $HOME/cloud-store-ut-buckets --address $s3_addr &
+
+        minio_pid="$!"
+        sleep 5
+
+        $jre/bin/java -cp ./lib/java/s3lib-test.jar com.logicblox.s3lib.TestRunner --keydir $keydir --endpoint $s3_endpoint
+      '';
+
+      installPhase = ''
+         # nothing to do here, but this job will fail if we don't produce
+         # an output directory for some reason....
+         mkdir -p $out
+      '';
+
+    };
+
+
   jobs = rec {
     source_tarball = 
       pkgs.releaseTools.sourceTarball { 
@@ -48,84 +118,15 @@ let
         propagatedBuildInputs = [ jdk ];
       };
 
-    build_minio = pkgs.buildGoPackage rec {
-      name = "minio";
-      goPackagePath = "github.com/minio/minio";
-      rev = "e2aba9196f849c458303aff42d2d6ea3e3ea8904";
-
-      src = pkgs.fetchgit {
-        inherit rev;
-        url = "https://github.com/minio/minio.git";
-        sha256 = "1iixpxcyhfa1lln3qd4xpnmjpbkf0zicj1irk21wqjqkac3rar0s";
-      };
-    };
-
-    test =
-      pkgs.stdenv.mkDerivation {
-        name = "${name}-test";
-        src = build.out;
-        jre = "${jdk.jre}";
-        buildInputs =
-          [
-            # pkgs.minio
-            # pkgs.minio-client
-            pkgs.awscli
-            build
-            build_minio
-          ];
-        buildPhase = ''
-          set -e
-          minio_pid=""
-
-          cleanup_minio()
-          {
-            if [[ -n "$minio_pid" ]];
-            then
-              kill $minio_pid
-            fi
-          }
-
-          trap cleanup_minio EXIT
-
-          export HOME=$(pwd)
-
-          # these are dummy keys created just for the tests
-          export AWS_ACCESS_KEY_ID=9NLZKB4SPH2OP5L845XE
-          export AWS_SECRET_ACCESS_KEY=rvzui7pQS0PI1aAOhtTHWVmJvhMY+b9xSw7arAbC
-
-          s3_addr="127.0.0.1:9000"
-          s3_endpoint="http://$s3_addr/"
-          keydir="$(pwd)/cloud-store-ut-keys"
-          mkdir -p $keydir
-
-          #minio_bin="minio"
-          #minio_bin="./bin/minio.latest"
-          minio_bin="${build_minio}/bin/minio"
-          $minio_bin -h
-          $minio_bin version
-
-          MINIO_ACCESS_KEY=$AWS_ACCESS_KEY_ID MINIO_SECRET_KEY=$AWS_SECRET_ACCESS_KEY \
-            $minio_bin server $HOME/cloud-store-ut-buckets --address $s3_addr &
-
-          minio_pid="$!"
-          sleep 5
-
-          $jre/bin/java -cp ./lib/java/s3lib-test.jar com.logicblox.s3lib.TestRunner --keydir $keydir --endpoint $s3_endpoint
-        '';
-
-        installPhase = ''
-           # nothing to do here, but this job will fail if we don't produce
-           # an output directory for some reason....
-           mkdir -p $out
-        '';
-
-      };
-
     binary_tarball =
       builder_config.release_helper {
         inherit name build;
         unixify = "bin/cloud-store bin/s3tool";
         };
+
+    # minio fails to build on osx
+    test = (if stdenv.system == "x86_64-linux" then test_linux else null);
+
     };
 
 in jobs

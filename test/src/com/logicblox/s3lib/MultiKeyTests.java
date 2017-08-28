e38b9f5d65505c82a7996bc2ee0fc64523f72854
George Kollias <george.kollias@logicblox.com>
2017-08-28 20:33:15 +0300

    New/updated tests for multiple encryption keys

--- test/src/com/logicblox/s3lib/MultiKeyTests.java
+++ test/src/com/logicblox/s3lib/MultiKeyTests.java
@@ -2,7 +2,11 @@ package com.logicblox.s3lib;
 
 import java.io.File;
 import java.net.URI;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.Map;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import junit.framework.Assert;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -49,10 +53,11 @@ public class MultiKeyTests
   {
     // generate 2 new public/private key pairs
     File keydir = TestUtils.createTmpDir(true);
-    String key1 = "cloud-store-ut-1";
-    String key2 = "cloud-store-ut-2";
-    createKey(key1, keydir);
-    createKey(key2, keydir);
+    String[] keys = {"cloud-store-ut-1", "cloud-store-ut-2",
+                     "cloud-store-ut-3", "cloud-store-ut-4"};
+
+    for(String key : keys)
+      createKey(key, keydir);
     TestUtils.setKeyProvider(keydir);
 
     // capture files currently in test bucket
@@ -63,7 +68,7 @@ public class MultiKeyTests
     // create a small file and upload
     File toUpload = TestUtils.createTextFile(100);
     URI dest = TestUtils.getUri(_testBucket, toUpload, rootPrefix);
-    S3File f = TestUtils.uploadEncryptedFile(toUpload, dest, key1);
+    S3File f = TestUtils.uploadEncryptedFile(toUpload, dest, keys[0]);
     Assert.assertNotNull(f);
 
     // make sure file was uploaded
@@ -80,17 +85,24 @@ public class MultiKeyTests
     Assert.assertTrue(TestUtils.compareFiles(toUpload, f.getLocalFile()));
     dlTemp.delete();
 
-    // add a second key to the file
-    f = _client.addEncryptionKey(_testBucket, objKey, key2).get();
-    Assert.assertNotNull(f);
+    // add the rest of the keys to the file
+    for(int i = 1; i < keys.length; i++)
+    {
+      f = _client.addEncryptionKey(_testBucket, objKey, keys[i]).get();
+      Assert.assertNotNull(f);
+    }
 
-    // hide key1 and key2, download should fail
+    // hide all .pem files
     File hidden = TestUtils.createTmpDir(true);
-    String fn1 = key1 + ".pem";
-    String fn2 = key2 + ".pem";
-    TestUtils.moveFile(fn1, keydir, hidden);
-    TestUtils.moveFile(fn2, keydir, hidden);
+    for(String key : keys)
+    {
+        String fn = key + ".pem";
+        TestUtils.moveFile(fn, keydir, hidden);
+    }
+
+    // dl should fail
     String msg = null;
+    dlTemp = TestUtils.createTmpFile();
     try
     {
       TestUtils.downloadFile(dest, dlTemp);
@@ -99,54 +111,104 @@ public class MultiKeyTests
     catch(Throwable t)
     {
       // expected
-      Assert.assertTrue(t.getMessage().contains("No eligible private key found")
-                                  || t.getMessage().contains("is not available to decrypt"));
+      Assert.assertTrue(t.getMessage().contains("No eligible private key found"));
     }
     Assert.assertNull(msg);
     Assert.assertFalse(dlTemp.exists());
 
-    // replace key2, download should work
-    TestUtils.moveFile(fn2, hidden, keydir);
-    f = TestUtils.downloadFile(dest, dlTemp);
-    Assert.assertNotNull(f.getLocalFile());
-    Assert.assertTrue(dlTemp.exists());
-    Assert.assertTrue(TestUtils.compareFiles(toUpload, f.getLocalFile()));
-    dlTemp.delete();
-
-    // hide key2 and replace key1, dl should work
-    TestUtils.moveFile(fn1, hidden, keydir);
-    TestUtils.moveFile(fn2, keydir, hidden);    
-    f = TestUtils.downloadFile(dest, dlTemp);
-    Assert.assertNotNull(f.getLocalFile());
-    Assert.assertTrue(dlTemp.exists());
-    Assert.assertTrue(TestUtils.compareFiles(toUpload, f.getLocalFile()));
-    dlTemp.delete();
-
-    // remove key1 from the file, dl should fail
-    f = _client.removeEncryptionKey(_testBucket, objKey, key1).get();
-    Assert.assertNotNull(f);
-    try
+    int []keyOrder = {2, 0, 3};
+    for(int ki : keyOrder)
     {
-      TestUtils.downloadFile(dest, dlTemp);
-      msg = "Expected download error (key not found)";
-    }
-    catch(Throwable t)
-    {
-      // expected
-      Assert.assertTrue(t.getMessage().contains("is not available to decrypt"));
-    }
-    Assert.assertNull(msg);
-    Assert.assertFalse(dlTemp.exists());
+      // bring back specific .pem file, dl should succeed
+      String fn = keys[ki] + ".pem";
+      TestUtils.copyFile(fn, hidden, keydir);
+      dlTemp = TestUtils.createTmpFile();
+      f = TestUtils.downloadFile(dest, dlTemp);
+      Assert.assertNotNull(f.getLocalFile());
+      Assert.assertTrue(dlTemp.exists());
+      Assert.assertTrue(TestUtils.compareFiles(toUpload, f.getLocalFile()));
+      dlTemp.delete();
 
-    // replace key2, dl should work
-    TestUtils.moveFile(fn2, hidden, keydir);
+      // remove key from the file, dl should fail
+      f = _client.removeEncryptionKey(_testBucket, objKey, keys[ki]).get();
+      Assert.assertNotNull(f);
+      dlTemp = TestUtils.createTmpFile();
+      try
+      {
+        TestUtils.downloadFile(dest, dlTemp);
+        msg = "Expected download error (key not found)";
+      }
+      catch(Throwable t)
+      {
+        // expected
+        if (ki == 3)
+          Assert.assertTrue(t.getMessage().contains("is not available to decrypt"));
+        else
+          Assert.assertTrue(t.getMessage().contains("No eligible private key found"));
+      }
+      Assert.assertNull(msg);
+      Assert.assertFalse(dlTemp.exists());
+
+      Files.delete((new File(keydir, fn)).toPath());
+    }
+
+    // use key1, dl should work
+    TestUtils.copyFile(keys[1] + ".pem", hidden, keydir);
     f = TestUtils.downloadFile(dest, dlTemp);
     Assert.assertNotNull(f.getLocalFile());
     Assert.assertTrue(dlTemp.exists());
     Assert.assertTrue(TestUtils.compareFiles(toUpload, f.getLocalFile()));
     dlTemp.delete();
   }
-  
+
+  @Test
+  public void testPartialKeys()
+    throws Throwable
+  {
+    // generate a new public/private key pair
+    String key1 = "cloud-store-ut-1";
+    String key2 = "cloud-store-ut-2";
+    File keydir = TestUtils.createTmpDir(true);
+    String[] keys1 = TestUtils.createEncryptionKey(keydir, key1);
+    String privateKey1 = keys1[0];
+    String publicKey1 = keys1[1];
+    String[] keys2 = TestUtils.createEncryptionKey(keydir, key2);
+    String privateKey2 = keys2[0];
+    String publicKey2 = keys2[1];
+    TestUtils.setKeyProvider(keydir);
+
+    String rootPrefix = TestUtils.addPrefix("");
+
+    // create a small file and upload
+    File toUpload = TestUtils.createTextFile(100);
+    URI dest = TestUtils.getUri(_testBucket, toUpload, rootPrefix);
+    S3File f = TestUtils.uploadEncryptedFile(toUpload, dest, key1);
+    Assert.assertNotNull(f);
+
+    // the private part of key1 & the public part of key2 should be
+    // sufficient to add key2
+    String key1FileName = key1 + ".pem";
+    TestUtils.writeToFile(privateKey1, new File(keydir, key1FileName));
+
+    String key2FileName = key2 + ".pem";
+    TestUtils.writeToFile(publicKey2, new File(keydir, key2FileName));
+
+    String objKey = rootPrefix + toUpload.getName();
+    f = _client.addEncryptionKey(_testBucket, objKey, key2).get();
+    Assert.assertNotNull(f);
+
+    // try to decrypt with key2's private part
+    TestUtils.writeToFile(privateKey2, new File(keydir, key2FileName));
+    File hidden = TestUtils.createTmpDir(true);
+    TestUtils.moveFile(key1FileName, keydir, hidden);
+
+    File dlTemp = TestUtils.createTmpFile();
+    f = TestUtils.downloadFile(dest, dlTemp);
+    Assert.assertNotNull(f.getLocalFile());
+    Assert.assertTrue(dlTemp.exists());
+    Assert.assertTrue(TestUtils.compareFiles(toUpload, f.getLocalFile()));
+    dlTemp.delete();
+  }
 
   @Test
   public void testDuplicateKey()
@@ -406,6 +468,51 @@ public class MultiKeyTests
       Assert.assertTrue(t.getMessage().contains("No more than 4 keys are allowed"));
     }
     Assert.assertNull(msg);
+  }
+
+
+  @Test
+  public void testDownloadNoPubkeyHash()
+    throws Throwable
+  {
+    // generate 2 new public/private key pairs
+    File keydir = TestUtils.createTmpDir(true);
+    String key1 = "cloud-store-ut-1";
+    createKey(key1, keydir);
+    TestUtils.setKeyProvider(keydir);
+    String rootPrefix = TestUtils.addPrefix("");
+
+    // create a small file and upload
+    File toUpload = TestUtils.createTextFile(100);
+    URI dest = TestUtils.getUri(_testBucket, toUpload, rootPrefix);
+    S3File f = TestUtils.uploadEncryptedFile(toUpload, dest, key1);
+    Assert.assertNotNull(f);
+
+    // remove "s3tool-pubkey-hash" from metadata to simulate files
+    // uploaded by older cloud-store versions
+    String objKey = rootPrefix + toUpload.getName();
+    ObjectMetadata destMeta = _client.exists(dest).get();
+    Assert.assertNotNull(destMeta);
+    Map<String,String> destUserMeta = destMeta.getUserMetadata();
+    Assert.assertNotNull(destUserMeta);
+    Assert.assertTrue(destUserMeta.containsKey("s3tool-pubkey-hash"));
+    destUserMeta.remove("s3tool-pubkey-hash");
+    TestUtils.updateObjectUserMetadata(_testBucket, objKey, destUserMeta);
+
+    // make sure "s3tool-pubkey-hash" has been removed
+    destMeta = _client.exists(dest).get();
+    Assert.assertNotNull(destMeta);
+    destUserMeta = destMeta.getUserMetadata();
+    Assert.assertNotNull(destUserMeta);
+    Assert.assertFalse(destUserMeta.containsKey("s3tool-pubkey-hash"));
+
+    // download the file and compare it with the original
+    File dlTemp = TestUtils.createTmpFile();
+    f = TestUtils.downloadFile(dest, dlTemp);
+    Assert.assertNotNull(f.getLocalFile());
+    Assert.assertTrue(dlTemp.exists());
+    Assert.assertTrue(TestUtils.compareFiles(toUpload, f.getLocalFile()));
+    dlTemp.delete();
   }
 
 

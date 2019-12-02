151ac9a9294431e2881f4daa714d13cebcac8a8b
George Kollias <george.kollias@logicblox.com>
2019-12-02 13:22:23 +0200

    Minor imports cleanup

--- src/com/logicblox/cloudstore/Utils.java
+++ src/com/logicblox/cloudstore/Utils.java
@@ -26,10 +26,8 @@ import org.apache.log4j.PatternLayout;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;

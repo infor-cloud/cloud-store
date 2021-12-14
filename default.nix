50d7195225be3706f630377b7456cbc8e94ff351
Wes Hunter <wes.hunter@logicblox.com>
2021-12-14 12:19:46 -0500

    fixed cloud-store nix build to work with nixpkgs 19.03

--- default.nix
+++ default.nix
@@ -80,7 +80,7 @@ let
     pkgs.stdenv.mkDerivation {
       name = "${name}-test";
       src = jobs.build.out;
-      /* jre = "${jdk.jre}"; */
+      jre = "${jdk}";
       buildInputs =
         [
           pkgs.awscli
@@ -122,7 +122,7 @@ let
         minio_pid="$!"
         sleep 5
 
-        $jdk/bin/java -cp ./lib/java/cloudstore-test.jar com.logicblox.cloudstore.TestRunner --keydir $keydir --endpoint $s3_endpoint
+        $jre/bin/java -cp ./lib/java/cloudstore-test.jar com.logicblox.cloudstore.TestRunner --keydir $keydir --endpoint $s3_endpoint
       '';
 
       installPhase = ''
